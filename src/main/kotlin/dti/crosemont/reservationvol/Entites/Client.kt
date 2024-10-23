f8dc060b0553ce2a91140861c6f0d11a05214a0a
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-10-23 14:20:54 -0400

    Ajout du modèle de Client

--- src/main/kotlin/dti/crosemont/reservationvol/Entites/Client.kt
+++ src/main/kotlin/dti/crosemont/reservationvol/Entites/Client.kt
@@ -1,5 +1,11 @@
 package dti.crosemont.reservationvol.Entites
 
 class Client(
-    val id : Int
+    val id : Int,
+    val nom : String,
+    val prénom : String,
+    val adresse : String,
+    val numéroPasseprt : String,
+    val email : String?,
+    val numéroTéléphone : String?,
 )
