fb111970cc83015b370397fec2362835a11809aa
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-10-23 14:13:02 -0400

    Retour a JDBC

--- src/main/kotlin/dti/crosemont/reservationvol/Entites/Client.kt
+++ src/main/kotlin/dti/crosemont/reservationvol/Entites/Client.kt
@@ -1,10 +1,5 @@
 package dti.crosemont.reservationvol.Entites
 
-import jakarta.persistence.*
-
-@Entity
 class Client(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id : Int
 )
