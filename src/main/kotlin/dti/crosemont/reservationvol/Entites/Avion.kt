4da4bfa3a81201ce88106489287ea4f949c9aeea
Lestat LouisSeize <llouisseize@ordralphabetix>
2024-10-24 13:27:28 -0400

    Ajout du modèle Avion et Reservation et une classe Enum.

---
+++ src/main/kotlin/dti/crosemont/reservationvol/Entites/Avion.kt
@@ -0,0 +1,8 @@
+package dti.crosemont.reservationvol.Entites
+
+data class Avion (
+    val type: String,
+    val nb_siège_économique: Int,
+    val nb_siège_affaire: Int,
+    val nb_siège_première: Int
+)
