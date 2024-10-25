f5c615e0d3b76fe2314a167c2d43b401e6d70d97
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-10-24 21:03:56 -0400

    Ajout du fichier de controleur clients

---
+++ src/main/kotlin/dti/crosemont/reservationvol/Controleurs/ClientsControleur.kt
@@ -0,0 +1,6 @@
+package dti.crosemont.reservationvol.Controleurs
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController @RequestMapping("/clients") class ClientsControleur {}
