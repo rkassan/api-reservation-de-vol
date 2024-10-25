573bcb7dbce13cf1124cbedb2b1bd0c97ad5f81e
Teddy Cabrel Ekeu <tekeu@ordralphabetix>
2024-10-24 21:38:15 -0400

    Ajout des signatures des méthodes post et delete pour client

--- src/main/kotlin/dti/crosemont/reservationvol/Controleurs/ClientsControleur.kt
+++ src/main/kotlin/dti/crosemont/reservationvol/Controleurs/ClientsControleur.kt
@@ -3,27 +3,39 @@ package dti.crosemont.reservationvol.Controleurs
 import dti.crosemont.reservationvol.Entites.Client
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/clients")
 class ClientsControleur {
 
-    @GetMapping
-    fun obtenirToutLesClients(@RequestParam("keyword") keyword : String?): ResponseEntity<List<Client>> =
-            ResponseEntity(HttpStatus.NOT_IMPLEMENTED)`
-    
-    @GetMapping("/{id}")
-    fun obtenirUnClientParId(@PathVariable id : Int): ResponseEntity<Client> =
-            ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
-    
-    @DeleteMapping("/{id}")
-    fun supprimerUnClientParId(@PathVariable id : Int): ResponseEntity<HttpStatus> = 
-            ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
-    
+        @GetMapping
+        fun obtenirToutLesClients(
+                @RequestParam("keyword") keyword: String?
+        ): ResponseEntity<List<Client>> = ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
+
+        @GetMapping("/{id}")
+        fun obtenirUnClientParId(@PathVariable id: Int): ResponseEntity<Client> =
+                ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
+
+        @PostMapping
+        fun ajouterClient(@RequestBody client: Client): ResponseEntity<Client> =
+                ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
+
+        @PutMapping("/{id}")
+        fun ModifierClient(
+                @PathVariable id: Int,
+                @RequestBody client: Client
+        ): ResponseEntity<Client> = ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
+
+        @DeleteMapping("/{id}")
+        fun supprimerUnClientParId(@PathVariable id: Int): ResponseEntity<HttpStatus> =
+                ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
 }
