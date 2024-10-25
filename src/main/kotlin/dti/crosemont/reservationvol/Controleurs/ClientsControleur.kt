7f1c25addb5535954a60eace0e3c948607a75921
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-10-25 08:07:41 -0400

    ajout de required false dans le get tout les clients

--- src/main/kotlin/dti/crosemont/reservationvol/Controleurs/ClientsControleur.kt
+++ src/main/kotlin/dti/crosemont/reservationvol/Controleurs/ClientsControleur.kt
@@ -17,24 +17,26 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/clients")
 class ClientsControleur {
 
-    @GetMapping
-    fun obtenirToutLesClients(
-            @RequestParam("keyword") keyword: String?
-    ): ResponseEntity<List<Client>> = ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
+        @GetMapping
+        fun obtenirToutLesClients(
+                @RequestParam(name = "keyword", required = false) keyword: String?
+        ): ResponseEntity<List<Client>> = ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
 
-    @GetMapping("/{id}")
-    fun obtenirUnClientParId(@PathVariable id: Int): ResponseEntity<Client> =
-            ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
+        @GetMapping("/{id}")
+        fun obtenirUnClientParId(@PathVariable id: Int): ResponseEntity<Client> =
+                ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
 
-    @PostMapping
-    fun ajouterClient(@RequestBody client: Client): ResponseEntity<Client> =
-            ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
+        @PostMapping
+        fun ajouterClient(@RequestBody client: Client): ResponseEntity<Client> =
+                ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
 
-    @PutMapping("/{id}")
-    fun ModifierClient(@PathVariable id: Int, @RequestBody client: Client): ResponseEntity<Client> =
-            ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
+        @PutMapping("/{id}")
+        fun ModifierClient(
+                @PathVariable id: Int,
+                @RequestBody client: Client
+        ): ResponseEntity<Client> = ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
 
-    @DeleteMapping("/{id}")
-    fun supprimerUnClientParId(@PathVariable id: Int): ResponseEntity<HttpStatus> =
-            ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
+        @DeleteMapping("/{id}")
+        fun supprimerUnClientParId(@PathVariable id: Int): ResponseEntity<HttpStatus> =
+                ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
 }
