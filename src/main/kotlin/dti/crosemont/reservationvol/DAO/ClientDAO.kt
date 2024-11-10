35f4caaa8e79405a49987bf184a1355257b9861d
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-11-09 21:43:50 -0500

    ajout des méthode DAO pour obtenir les clients

--- src/main/kotlin/dti/crosemont/reservationvol/DAO/ClientDAO.kt
+++ src/main/kotlin/dti/crosemont/reservationvol/DAO/ClientDAO.kt
@@ -4,5 +4,5 @@ import dti.crosemont.reservationvol.Entites.Client
 import dti.crosemont.reservationvol.DAO
 
 interface ClientDAO : DAO<Client>{
-
+    fun chercherParMotCle(motClé : String) : List<Client>
 }
