27162002b045ae405cf1d8d7a97bc8c0bb918ca7
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-11-14 11:39:53 -0500

    Correction type de retour de obtenir clients par id a non nullable

--- src/main/kotlin/dti/crosemont/reservationvol/Domaine/Service/ClientsService.kt
+++ src/main/kotlin/dti/crosemont/reservationvol/Domaine/Service/ClientsService.kt
@@ -10,6 +10,6 @@ class ClientsService( private val dao : ClientDAO) {
 
     fun obtenirToutLesClient() : List<Client> = dao.chercherTous()
     fun obtenirClientsParMotCle( motClé : String ) : List<Client> = dao.chercherParMotCle( motClé )
-    fun obtenirParId( id : Int ) : Client? =
+    fun obtenirParId( id : Int ) : Client =
             dao.chercherParId( id ) ?: throw RessourceInexistanteException("Le client n'existe pas")
 }
