153dcd3ac95dc88fba4edd7684de68bb174ee49a
Teddy Cabrel Ekeu <tekeu@ordralphabetix>
2024-11-14 19:31:13 -0500

    ajout de methode dans l'interface

--- src/main/kotlin/dti/crosemont/reservationvol/AccesAuxDonnees/SourcesDeDonnees/AeroportDAO.kt
+++ src/main/kotlin/dti/crosemont/reservationvol/AccesAuxDonnees/SourcesDeDonnees/AeroportDAO.kt
@@ -5,4 +5,6 @@ import dti.crosemont.reservationvol.Domaine.Modele.Aeroport
 interface AeroportDAO : DAO<Aeroport> {
     fun chercherParCode(code: String): Aeroport?
     fun chercherParNom(nom: String): List<Aeroport>
+    fun modifier(aeroport: Aeroport): Aeroport
+    fun ajouter(aeroport: Aeroport): Aeroport
 }
