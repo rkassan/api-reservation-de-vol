414bfdd10099c576bfa187c3883e8fc9474104e9
Teddy Cabrel Ekeu <tekeu@ordralphabetix>
2024-11-14 20:12:06 -0500

    finission controlleur aeroport

--- src/main/kotlin/dti/crosemont/reservationvol/Domaine/Service/AeroportService.kt
+++ src/main/kotlin/dti/crosemont/reservationvol/Domaine/Service/AeroportService.kt
@@ -1,4 +1,5 @@
 package dti.crosemont.reservationvol.Domaine.Service
+import dti.crosemont.reservationvol.Controleurs.Exceptions.RequêteMalFormuléeException
 
 import dti.crosemont.reservationvol.AccesAuxDonnees.SourcesDeDonnees.AeroportDAO
 import dti.crosemont.reservationvol.Domaine.Modele.Aeroport
@@ -11,4 +12,21 @@ class AeroportService(private val dao: AeroportDAO) {
     fun obtenirAeroportParCode(code: String): Aeroport? = dao.chercherParCode(code)
     fun obtenirAeroportsParNom(nom: String): List<Aeroport> = dao.chercherParNom(nom)
     fun obtenirAeroportParId(id: Int): Aeroport? = dao.chercherParId(id)
+    fun supprimerUnAeroport( id : Int ) = dao.effacer( id )
+    fun ajouterAeroport(aeroport: Aeroport): Aeroport {
+        val resultat = dao.ajouter(aeroport)
+        if (resultat != null) {
+            return resultat
+        }
+        throw RequêteMalFormuléeException("L'ajout de l'aéroport a échoué")
+    }
+
+    fun modifierAeroport(aeroport: Aeroport): Aeroport {
+        val resultat = dao.modifier(aeroport)
+        if (resultat != null) {
+            return resultat
+        }
+        throw RequêteMalFormuléeException("La modification de l'aéroport a échoué")
+    }
+  
 }
